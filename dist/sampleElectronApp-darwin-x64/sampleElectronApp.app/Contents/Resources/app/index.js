3295d38b8459814591a65add28eba8924f7f9cbc
Masumi.M <masumi.lacrosse@live.jp>
2019-09-06 07:56:54 +0900

    First Commit

---
+++ dist/sampleElectronApp-darwin-x64/sampleElectronApp.app/Contents/Resources/app/index.js
@@ -0,0 +1,23 @@
+const electron = require("electron")
+const app = electron.app;
+const BrowserWindow = electron.BrowserWindow;
+
+let mainWindow;
+
+app.on('window-all-closed', () => {
+    if (process.platform != 'darwin') {
+        app.quit();
+    }
+});
+
+app.on('ready', () => {
+    mainWindow = new BrowserWindow({
+        width: 1200,
+        height: 800
+    });
+    mainWindow.loadURL('file://' + __dirname + '/index.html');
+
+    mainWindow.on('closed', () => {
+        mainWindow = null;
+    });
+});
